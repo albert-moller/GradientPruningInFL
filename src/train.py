a8f4955d818ffb261a51960c3869b8e5761d6399
markandrawes <markandrawes@hotmail.com>
2024-03-27 21:05:55 +0100

    first attempt and experiment with gradient pruning

--- src/train.py
+++ src/train.py
@@ -23,11 +23,27 @@ def validation(model, test_dataloader, device):
     accuracy = num_correct / total
     return accuracy
 
-def train_client(id, client_dataloader, global_model, num_local_epochs, lr, device, criterion, filtered_train_dataset):
+def prune_gradients(model, thres, alpha):
+    gradients = []
+    for param in model.parameters():
+        if param.grad is not None:
+            gradients += list(param.grad.cpu().data.abs().numpy().flatten())
+    threshold = np.percentile(gradients, thres)
+    for param in model.parameters():
+        if param.grad is not None:
+            grad_above_thresh = param.grad.data.abs() > threshold
+            param.grad.data[grad_above_thresh] *= alpha
+
+
+def train_client(id, client_dataloader, global_model, num_local_epochs, lr, device, criterion, filtered_train_dataset, prune=False):
     local_model = copy.deepcopy(global_model)
     local_model.to(device)
     local_model.train()
     optimizer = torch.optim.Adam(local_model.parameters(), lr=lr)
+
+    #parameters for pruning
+    thres = 30
+    alpha = 0.1
 
     for epoch in range(num_local_epochs):
         for (index, (img, label)) in enumerate(client_dataloader):
@@ -36,7 +52,9 @@ def train_client(id, client_dataloader, global_model, num_local_epochs, lr, devi
             predict = local_model(img)
             loss = criterion(predict, label)
             loss.backward()
-            # Optionally: prune_gradients(local_model)
+            # apply gradient pruning optionally
+            if prune:
+                prune_gradients(local_model, thres=thres, alpha=alpha)
             optimizer.step()        
 
         #Perform gradient inversion attack using iDLG:
@@ -78,7 +96,7 @@ def global_model_average(curr, next, scale):
             curr[key] = curr[key] + (next[key]*scale)  
     return curr
 
-def federated_learning_experiment(global_model, num_clients_per_round, num_local_epochs, lr, client_train_loader, max_rounds, device, criterion, test_dataloader, filtered_train_dataset):
+def federated_learning_experiment(global_model, num_clients_per_round, num_local_epochs, lr, client_train_loader, max_rounds, device, criterion, test_dataloader, filtered_train_dataset, prune=False):
     round_train_accuracy = []
     for round in range(max_rounds):
         print(f"Round {round} is starting")
@@ -90,7 +108,7 @@ def federated_learning_experiment(global_model, num_clients_per_round, num_local
 
         for index, client in enumerate(clients):
             print(f"round {round}, starting client {(index+1)}/{num_clients_per_round}, id: {client}")
-            local_model = train_client(client, client_train_loader[client], global_model, num_local_epochs, lr, device=device, criterion=criterion, filtered_train_dataset=filtered_train_dataset)
+            local_model = train_client(client, client_train_loader[client], global_model, num_local_epochs, lr, device=device, criterion=criterion, filtered_train_dataset=filtered_train_dataset, prune=prune)
             running_avg = global_model_average(running_avg, local_model.state_dict(), 1/num_clients_per_round) 
 
         global_model.load_state_dict(running_avg)
@@ -99,5 +117,3 @@ def federated_learning_experiment(global_model, num_clients_per_round, num_local
         round_train_accuracy.append(validation_accuracy)
     
     return round_train_accuracy
-
-
